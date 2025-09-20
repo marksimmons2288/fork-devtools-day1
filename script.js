4596637c1143928bff54e4f213354dad92fa7d38
mark simmons <marksimmons8415@yahoo.com>
2025-09-20 15:29:10 -0500

    debug the errors on js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ console.log(message);
 // Error 2: 
 console.log(document);
 let button = document.querySelector('#actionButton'); 
-
+console.log('Button#actionButton', button);
 button.addEventListener('click', eventHandler); 
 
 // Error 3: 
@@ -41,19 +41,19 @@ console.log('Sum is: ' + (number1 - number2));
 
 // Error 9: 
 let nullVar = null;
-console.log(nullVar.length);
+console.log(nullVar);
 
 // Error 10:
 let caseSensitiveVar = 'Case matters';
-console.log(CaseSensitiveVar);
+console.log(caseSensitiveVar);
 
 // Error 11:
 let importantData =
   "This string must be logged to the console. It's important!";
-console.log('importantData');
+console.log(importantData);
 
 // Error 12: 
-tryToCallFunction();
+//tryToCallFunction();
 
 // Error 13:
 let user = { name: 'Alice' };
@@ -61,7 +61,7 @@ console.log(user.name)
 
 // Error 14: 
 let someNumber = 123;
-console.log(textToUpper.toUpperCase()); 
+console.log(someNumber); 
 
 // Error 15: 
 //  There are 3 types of variables
@@ -77,9 +77,9 @@ console.log(textToUpper.toUpperCase());
 let greeting = "Hello, world!";
 
 // Error 16: 
-for (let i = 0; i < 5; i++); {
+for (let i = 0; i < 5; i++) {
     console.log(i);
-}
+  }
 
 // Error 17:
 let five = '5';
@@ -97,8 +97,9 @@ numbers.push(1);
 console.log(numbers);
 
 // Error 19:
-console.log(x);
 x = 5;
+console.log(x);
+
 
 // Error 20:
 let myString = 'Hello World';
